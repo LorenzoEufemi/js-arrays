833718c9b64c1e6cb406c91af145ca1d103b7733
LorenzoEufemi <lorenzoeufemi@gmail.com>
2024-10-30 16:40:59 +0100

    feat: finalizzazione arrays

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,10 +10,17 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.slice().reverse();
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter(name => name.length >= 5);
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const edIndex = teachers.indexOf("Ed");
+if (edIndex !== -1) {
+  teachers.splice(edIndex, 1);
+}
+console.log(teachers);
